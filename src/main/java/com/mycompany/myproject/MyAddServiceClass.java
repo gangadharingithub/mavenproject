dd2ad9465354d01c0660e41b901c16dee5049a5f
mgangadh <gangadhar.mamillapalli@gmail.com>
2015-03-28 18:05:28 +0530

    adding files

---
+++ src/main/java/com/mycompany/myproject/MyAddServiceClass.java
@@ -0,0 +1,10 @@
+package com.mycompany.myproject;
+
+public class MyAddServiceClass {
+	
+	public int add(int a,int b){
+		
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/mycompany/myproject/MyAddServiceClassTest.java
@@ -0,0 +1,27 @@
+package com.mycompany.myproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyAddServiceClassTest {
+	
+	MyAddServiceClass myAddServiceClass;
+
+	@Before
+	public void setUp() throws Exception {		
+		myAddServiceClass = new MyAddServiceClass();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		assertEquals(2+2, myAddServiceClass.add(2, 2));
+	}
+
+}
